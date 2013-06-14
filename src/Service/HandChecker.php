cfc1600d8839ad164ca04e4f553e487bb16d3e2c
Natxo <natxo@kodify.io>
2013-06-14 14:34:51 +0200

    Is flush check done

--- src/Service/HandChecker.php
+++ src/Service/HandChecker.php
@@ -1,7 +1,25 @@
 <?php
 namespace Service;
 
+use Entity\Hand;
+
 class HandChecker
 {
+    public function isFlush(Hand $hand)
+    {
+        $lastSuit = null;
+        foreach ($hand->getCards() as $card) {
+            if ($lastSuit == null) {
+                $lastSuit = $card->getSuit();
+            }
+            if ($card->getSuit() != $lastSuit) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 
 }
